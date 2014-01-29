543c3a3470201adf1995e6a9399d88bb85a4ab3d
David Boon <david.boon@gmail.com>
2014-01-29 15:45:44 -0500

    removed non-essential fields for the purpose of this test/demo.

--- src/main/scala/User.scala
+++ src/main/scala/User.scala
@@ -12,13 +12,7 @@ class User extends MongoRecord[User] with ObjectIdKey[User] with IndexedRecord[U
 
   def meta: MongoMetaRecord[User] = User
   object email extends StringField(this, 255)
-  object displayName extends StringField(this, 255)
-  object status extends StringField(this, 255)
-  object organization extends ObjectIdField(this)
   object loginCount extends LongField(this)
-  object groups extends MongoListField[User, ObjectId](this)
-  object createdOn extends DateTimeField(this)
-
   object visibility extends MongoMapField[User, VisibilityBson](this)
 
   // override def toString: String = {
@@ -33,16 +27,6 @@ object User extends User with MongoMetaRecord[User]  with JsonStringConversion[U
 
 class VisibilityBson extends BsonRecord[VisibilityBson]  {
   def meta: BsonMetaRecord[VisibilityBson] = VisibilityBson
-  object _id extends ObjectIdField(this)
-  object inProgress extends BooleanField(this)
-  object discovery extends StringField(this, 200)
-  object personal extends StringField(this, 200)
-  object relationship extends StringField(this, 200)
-  object team extends StringField(this, 200)
-  object development extends StringField(this, 200)
-  object action extends StringField(this, 200)
-  object discoveryCompleted extends BooleanField(this)
-  object accessedTimestamp extends StringField(this, 50)
   object visible extends BooleanField(this)
 }
 
